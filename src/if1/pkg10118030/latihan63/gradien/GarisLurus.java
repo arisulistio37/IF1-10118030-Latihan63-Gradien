403e97304a27dfb6f9ac3fe2b88616b6fa2396b2
arisulistio37 <arisulistio37@gmail.com>
2019-10-24 21:49:21 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan63/gradien/GarisLurus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan63.gradien;
+
+/**
+ *
+ * @author Ari Sulistio
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
---
+++ src/if1/pkg10118030/latihan63/gradien/IF110118030Latihan63Gradien.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan63.gradien;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ * Deskipsi program : program ini berisi tentang gradien garis 
+ */
+public class IF110118030Latihan63Gradien {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Koordinat koordinat1 = new Koordinat(2, 10, 5, 7);
+        System.out.println("Garis yang melalui titik ("+koordinat1.getX1()+","
+                +koordinat1.getY1()+") dan ("+koordinat1.getX2()+","
+                +koordinat1.getY2()+")");
+        System.out.println("memiliki gradien sebesar "
+                +koordinat1.hitungGradien());
+        
+        Koordinat koordinat2 = new Koordinat(5, 1, 3, 12);
+        System.out.println("Garis yang melalui titik ("+koordinat2.getX1()+","
+                +koordinat2.getY1()+") dan ("+koordinat2.getX2()+","
+                +koordinat2.getY2()+")");
+        System.out.println("memiliki gradien sebesar "
+                +koordinat2.hitungGradien());
+    }
+    
+}
